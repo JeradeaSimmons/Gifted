8841974a1b2cc8d238b569a41d6be21f26ed87f5
Casey Collins <collinscasey.homes@gmail.com>
2022-08-04 14:50:52 -0600

    update

--- app/Controllers/GiftsController.js
+++ app/Controllers/GiftsController.js
@@ -2,6 +2,15 @@ import { ProxyState } from "../AppState.js";
 import { Pop } from "../Utils/Pop.js";
 
 
+
+function _drawGifts() {
+    let template = ''
+    ProxyState.gifts.forEach(g => template += g.Template)
+    document.getElementById('').innerHTML = template
+}
+
+
+
 export class GiftsController {
     constructor() {
 
