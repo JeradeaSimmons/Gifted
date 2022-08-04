d6daa7a6ce70dc477d6e5b32df915516230fc935
Casey Collins <collinscasey.homes@gmail.com>
2022-08-04 15:43:33 -0600

    upDATE

--- app/Services/GiftsService.js
+++ app/Services/GiftsService.js
@@ -2,9 +2,12 @@ import { ProxyState } from "../AppState.js";
 import { giftsApi } from "./AxiosService.js";
 
 class GiftsService {
+    getGifts() {
 
-    const res = await giftsApi.put(`gifts/${ProxyState.player.id}`, ProxyState)
-    console.log('put request for gifts', res.data);
+        const res = await giftsApi.put(`gifts/${ProxyState.player.id}`, ProxyState)
+        console.log('put request for gifts', res.data);
+
+    }
 
 }
 
